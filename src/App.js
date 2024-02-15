ae285b977126b21268909d0951200b8473b4564a
Laith-Alhaddad <152159134+Laith-Alhaddad@users.noreply.github.com>
2024-02-15 06:53:13 +0000

    Student Card

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from './components/Card';
+import './App.css';
+
+function Card(){
+  return(
+    <div className='App'>
+      <header className='App-header'>
+        <Card />
+        <Card />
+      </header>
+    </div>
+  )
+
+}
+
+export default Card;
