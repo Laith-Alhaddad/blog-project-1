114e2ab0e72d2f545fbadb04f06f148d21c02374
Laith-Alhaddad <152159134+Laith-Alhaddad@users.noreply.github.com>
2024-03-04 07:59:36 +0000

    3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,41 @@
 import React from 'react'
 import { images } from '../constants'
 
+const NavItemInfo = [
+  {name: "Home"},
+  {name: "Articles"},
+  {name: "Pages"},
+  {name: "Pricing"},
+  {name: "Faq"},
+  
+]
+
+const NavItem = ([name]) => {
+  <li className='relative group'>
+              <a href="/" className='px-4 py-2'>Home</a>
+              <span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>/</span>
+              </li>
+}
+
 const Header = () => {
+  
   return (
     <section>
+      <header className='container mx-auto px-5 flex justify-between py-4 items-center'>
         <div>
           <img src={images.Logo} alt= "Logo"/>  
         </div>
-        <div>
-         <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Articles</a></li>
-            <li><a href="">Pages</a></li>
-            <li><a href="">Pricing</a></li>
-            <li><a href="">Faq</a></li>
+        <div className='flex gap-x-9 items-center'>
+         <ul className='flex gap-x-5 font-semibold'>
+        {NavItemInfo.map((item) =>(
+          <NavItem key={item.name}/>
+        ))}
          </ul>
-         <button>Sign in</button>
+         <button className='border-2 border-blue-500 px-6 py-3 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'>
+          Sign in
+          </button>
         </div>
+        </header>
     </section>
   )
 }
