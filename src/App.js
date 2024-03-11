352043e09674004b403ea63f6debf1ca3d041f95
Laith-Alhaddad <152159134+Laith-Alhaddad@users.noreply.github.com>
2024-03-11 08:35:28 +0000

    5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from './components/Card';
-import './App.css';
-import HomePage from './pages/HomePage';
+import "./App.css";
+import HomePage from "./pages/HomePage";
 
-function Card(){
-  return(
-    <div className='App font-opensans'>
-        <HomePage/>
+function App() {
+  return (
+    <div className="App font-opensans">
+      <HomePage />
     </div>
-  )
-
+  );
 }
 
-export default Card;
+export default App;
